f916cc508e3868d44e0d6cd4f0196cadbe029d1c
DEO-DECODE <d.jrajsingh81@gmail.com>
2022-07-07 18:19:31 +0530

    Our Department Section Added

---
+++ slider.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+
+    autoplay: {
+        display: 3000,
+        disableOnInteraction: false,
+  
+    },
+  
+    loop: true,
+  
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+  
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  });
+   
